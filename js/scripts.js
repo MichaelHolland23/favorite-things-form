1ecd192b0ef099566b8ff215527a1c184e7423ad
mike holland and senya webster <senya@>
2017-08-14 11:00:46 -0700

    working form with jquery vars and arrays

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function(){
+  $('#favoriteform').submit(function(event) {
+    event.preventDefault();
+    var favorite1 = $('#favorite1').val();
+    var favorite2 = $('#favorite2').val();
+    var favorite3 = $('#favorite3').val();
+    var favorite4 = $('#favorite4').val();
+    var favorite5 = $('#favorite5').val();
+
+    var favoriteArray = [favorite1, favorite2, favorite3, favorite4, favorite5];
+    var favoriteArray2 = favoriteArray['1'];
+    var favoriteArray3 = favoriteArray['0'];
+    var favoriteArray4 = favoriteArray['2'];
+
+    alert('Your Favorite Things Are: ' + favoriteArray2 + ', ' + favoriteArray3 + ', ' + favoriteArray4);
+  });
+});
